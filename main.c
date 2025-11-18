1a3a13f989ea3f6a0d346f50b4b6c7b1d8a0650f
Gardner Sullivan <gardnersullivan9@gmail.com>
2025-11-18 10:17:24 -0700

    addded some preperation stuff

--- main.c
+++ main.c
@@ -5,5 +5,9 @@
 #include <arpa/inet.h>
 
 int main(int argc, char *argv[]) {
-    
+
+    int port = atoi(argv[1]);
+    printf("Server will run on port %d\n", port);
+
+    return 0;
 }
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import socket
+# i added these comments not chatgpt
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("127.0.0.1", 8080))  # store as variables later, maybe use command line arguments for that
+
+s.send("message".encode()) # figure out a username system as well as always sending with a newline character which is \n
+
+s.close()
+
